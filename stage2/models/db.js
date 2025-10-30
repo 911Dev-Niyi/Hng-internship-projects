be7ed725d2f28a78703468ef487479762ce68e13
911Dev-Niyi <anjorinadeniyi811@gmail.com>
2025-10-30 20:32:31 +0100

    Added fix for railway DB connection timeout

--- stage2/models/db.js
+++ stage2/models/db.js
@@ -2,20 +2,17 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-let db;
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-try {
-  db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  console.log('✅ Connected to MySQL Database');
-} catch (err) {
-  console.error('❌ MySQL connection failed:', err.message);
-  process.exit(1); // stop the app if DB fails
-}
+console.log('✅ MySQL pool initialized');
 
 export { db };
